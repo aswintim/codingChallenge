83c1f001157a1bd683e9080cec03f688dd14604d
Aswin Timalsina <aswin.timalsina1@gmail.com>
2020-03-06 15:17:21 -0600

    designing part done

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-// app.use(exp.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/public/index.html'));
@@ -16,10 +15,10 @@ app.get('/register', function(req, res){
     res.sendFile(path.join(__dirname+'/public/registration.html'));
 })
 
-// app.get('/login',function(){
-//     app.send("login page");
-// }
-//     )
+app.get('/login',function(req, res){
+    res.sendFile(path.join(__dirname+'/public/login.html'));
+}
+    )
 
 
 app.listen(3030, function(){
