9787599b74782576010d8f8f9559956b3345d26b
Aswin Timalsina <aswin.timalsina1@gmail.com>
2020-03-07 16:37:56 -0600

    work fine login and registration

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var exp = require("express");
 var app = exp();
-var path = require('path');
-var axios = require('axios');
+var path = require("path");
+
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -14,31 +14,12 @@ app.get('/', function(req, res){
 
 app.get('/register', function(req, res){
     res.sendFile(path.join(__dirname+'/public/registration.html'));
-<<<<<<< HEAD
-    res.send("Yup!!")
-})
-
-// app.post('/appendRegister', function(req, res){
-//     // var url = "http://ec2-3-14-152-181.us-east-2.compute.amazonaws.com/api/data/adduser?" + "username=" + req.username + "&password=" + req.password;
-   
-// })
-
-// app.get('/submit', function(){
-
-// })
-
-// app.get('/login',function(){
-//     app.send("login page");
-// }
-//     )
-=======
 })
 
 app.get('/login',function(req, res){
     res.sendFile(path.join(__dirname+'/public/login.html'));
 }
     )
->>>>>>> login-solved
 
 
 app.listen(3030, function(){
